f247f0edc5e99a54fded3fdc24dae25d71e6c72d
jun0wanan <jun0wanan@163.com>
2020-11-21 17:01:26 +0800

    master

--- utils.py
+++ utils.py
@@ -102,10 +102,38 @@ def bug_free():
     except:
 
 def to_Device():
-    
+
     def todevice(tensor, device):
         if isinstance(tensor, list) or isinstance(tensor, tuple):
             assert isinstance(tensor[0], torch.Tensor)
             return [todevice(t, device) for t in tensor]
         elif isinstance(tensor, torch.Tensor):
             return tensor.to(device)
+
+def init_modal_weight():
+    def init_modules(modules, w_init='kaiming_uniform'):
+        if w_init == "normal":
+            _init = init.normal_
+        elif w_init == "xavier_normal":
+            _init = init.xavier_normal_
+        elif w_init == "xavier_uniform":
+            _init = init.xavier_uniform_
+        elif w_init == "kaiming_normal":
+            _init = init.kaiming_normal_
+        elif w_init == "kaiming_uniform":
+            _init = init.kaiming_uniform_
+        elif w_init == "orthogonal":
+            _init = init.orthogonal_
+        else:
+            raise NotImplementedError
+        for m in modules:
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear)):
+                _init(m.weight)
+                if m.bias is not None:
+                    torch.nn.init.zeros_(m.bias)
+            if isinstance(m, (nn.LSTM, nn.GRU)):
+                for name, param in m.named_parameters():
+                    if 'bias' in name:
+                        nn.init.zeros_(param)
+                    elif 'weight' in name:
+                        _init(param)
